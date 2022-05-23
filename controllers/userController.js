8d07ce5d2b6876746dbf4f43e4adb12d62170d57
M A Razvi <abuzarflw@gmail.com>
2022-05-22 23:26:43 -0700

    Write function to update user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const {User, Thought} = require("../models/");
 
+//Get all users
 async function getAllUsers() {
     try {
         const users = await User.find();
@@ -10,6 +11,7 @@ async function getAllUsers() {
     }
 }
 
+//Get User by ID
 async function getOneUser(req, res) {
     try {
         const singleUser = await User.findOne({ _id: req.params.userId }).select('-__v');
@@ -26,6 +28,7 @@ async function getOneUser(req, res) {
     } 
 }
 
+//Create new user
 async function createUser(req, res) {
     try {
         const newUser= await User.create(req.body);
@@ -37,4 +40,24 @@ async function createUser(req, res) {
     }
 }
 
-module.exports = {getAllUsers, getOneUser, createUser}
+//update a user
+async function updateUser(req, res) {
+    try {
+        const updatedUser = await User.findOneAndUpdate(
+            {_id: req.params.userId},
+            {$set: req.body},
+            {runValidators: true, new: true}
+        )
+        if(updatedUser) {
+            res.json(updatedUser);
+        }
+        else {
+            res.status(404).json({message: "User not found!"});
+        }
+    } catch (error) {
+        res.status(500).json(error);
+        console.log(error);
+    }
+}
+
+module.exports = {getAllUsers, getOneUser, createUser, updateUser};
