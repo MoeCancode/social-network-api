cc238aa735d9defe5b30038fe369b8ddb8373638
M A Razvi <abuzarflw@gmail.com>
2022-05-22 18:33:42 -0700

    Define User schema and Model

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectionString = process.env.MONGODB_URI || "mongodb://localhost:27017/socialMediaDB";
 
-connect(connectionString, {
+mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+    const totalFriends =  this.friends.length;
+    return totalFriends;
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User; 
