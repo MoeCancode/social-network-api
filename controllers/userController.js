9e49f54154952091bf0b168904b2e15cbc16ebf7
M A Razvi <abuzarflw@gmail.com>
2022-05-23 00:48:55 -0700

    Write functions to add friends and delete users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const {User, Thought} = require("../models/");
+const { where, findOneAndUpdate } = require("../models/User");
 
 //Get all users
 async function getAllUsers() {
@@ -60,4 +61,57 @@ async function updateUser(req, res) {
     }
 }
 
-module.exports = {getAllUsers, getOneUser, createUser, updateUser};
+//Delete user
+async function deleteUser(req, res) {
+    try {
+        const toBeDeletedUser = await User.findOne(
+            {_id: req.params.userId},
+        ); 
+
+        if(toBeDeletedUser) {
+            while(toBeDeletedUser.thoughts.length > 0) {
+                toBeDeletedUser.thoughts.pop();
+            }
+            const sameUser = await User.findOneAndDelete(
+                {_id: req.params.userId}
+            ); 
+            res.json({message: "User deleted!"});
+        }
+        else {
+            res.status(404).json({message: "User not found!"})
+        }
+    } catch (error) {
+       res.status(500).json(error);
+       console.log(error); 
+    }
+}
+
+//Add friend 
+async function addFriend(req, res) {
+    try {
+        const currentUser = await User.findOne(
+            {_id: req.params.userId}
+        );
+        const friendUser = await User.findOne(
+            {_id: req.params.friendId}
+        )
+    
+        if(currentUser && friendUser) {
+            const ourUser = await User.findOneAndUpdate(
+                {_id: req.params.id},
+                { $push: { friends: friendUser._id } },
+                { runValidators: true, new: true }
+            );
+            res.json({message: "Friend Added!"})
+        }
+        else {
+            res.status(404).json({message: "User of Friend not found!"})
+        }
+    } catch (error) {
+        res.status(500).json(error);
+        console.log(error);
+    }
+}
+
+
+module.exports = {getAllUsers, getOneUser, createUser, updateUser, deleteUser, addFriend};
