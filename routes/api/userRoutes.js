075d6ea4371fcd0fa0e82d88b2c37755dd7809a5
M A Razvi <abuzarflw@gmail.com>
2022-05-23 19:52:11 -0700

    Link user functions to user routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+//Import functions made for users
 const userfunctions = require("../../controllers/userController");
 
 // methods for /api/users
 router.route('/').get(userfunctions.getAllUsers).post(userfunctions.createUser);
+
+//methods for /api/users/:userId
+router.route('/:userId').get(userfunctions.getOneUser).put(userfunctions.updateUser).delete(userfunctions.deleteUser);
+
+//methods for /api/users/:userId/friends
+router.route('/:userId/friends').post(userfunctions.addFriend);
+
+//Methods for /api/users/:userId/friends/friendId
+router.route('/:userId/friends/:friendId').delete(userfunctions.removeFriend);
 
 module.exports = router;
